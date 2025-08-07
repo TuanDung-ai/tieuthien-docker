4e984f858e074b60f38da78adbc67eddc475bcfe
TuanDung-ai <tuandung054@gmail.com>
2025-08-07 21:15:38 +0700

    Update register_handlers.py

--- handlers/register_handlers.py
+++ handlers/register_handlers.py
@@ -1,11 +1,19 @@
 # handlers/register_handlers.py
 from telegram.ext import Application, CommandHandler, MessageHandler, CallbackQueryHandler, filters
-from .start import start_command, help_command
-from modules.memory_manager import handle_message, handle_callback_query
+from handlers.commands import (
+    cmd_ghi_nho, cmd_xem_nho, cmd_xoa_nho,
+    cmd_xoa_tatca, cmd_tro_giup
+)
+from handlers.message_handler import handle_message
+from modules.memory_manager import handle_callback_query
+
 
 def register_handlers(app: Application):
-    app.add_handler(CommandHandler("start", start_command))
-    app.add_handler(CommandHandler("help", help_command))
+    app.add_handler(CommandHandler("ghi_nho", cmd_ghi_nho))
+    app.add_handler(CommandHandler("xem_nho", cmd_xem_nho))
+    app.add_handler(CommandHandler("xoa_nho", cmd_xoa_nho))
+    app.add_handler(CommandHandler("xoa_tatca", cmd_xoa_tatca))
+    app.add_handler(CommandHandler("tro_giup", cmd_tro_giup))
+
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     app.add_handler(CallbackQueryHandler(handle_callback_query))
-
