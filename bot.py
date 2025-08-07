49b1843492746b2baefba6838ba79818f24d013d
TuanDung-ai <tuandung054@gmail.com>
2025-08-07 22:34:07 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import threading
 from fastapi import FastAPI
 import uvicorn
 from telegram.ext import Application
-from config import TELEGRAM_BOT_TOKEN, PORT
+from config import BOT_TOKEN, PORT
 from handlers.register_handlers import register_handlers
 from core.logging_config import setup_logging
 
@@ -34,10 +34,10 @@ def run_uvicorn():
 
 # Hàm khởi động bot
 def main():
-    if not TELEGRAM_BOT_TOKEN:
+    if not BOT_TOKEN:
         raise ValueError("TELEGRAM_BOT_TOKEN is missing.")
 
-    app_bot = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
+    app_bot = Application.builder().token(BOT_TOKEN).build()
     register_handlers(app_bot)
     print("✅ Bot is running...")
     app_bot.run_polling()
