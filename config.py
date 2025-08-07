7d94f364874df17711a6c6e5402824fdf9d7df15
TuanDung-ai <tuandung054@gmail.com>
2025-08-07 17:29:26 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
-# config.py
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
 PORT = int(os.getenv("PORT", 8080))
-
